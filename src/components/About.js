09992c7af0be93d94e95a17a78bb15061780dbdb
Oraan Levi <oraanlevi1@gmail.com>
2023-06-13 16:53:05 -0400

    saving changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,35 @@
 import React from 'react';
 
 function About() {
-    return(
+  return (
     <section className="title">
       <h1 class="name"> ABOUT ME: Oraan Levi</h1>
       <hr></hr>
-      
+
       <div className="row justify-content-center">
         <div className="col-10" id="about-section">
-        <a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/FH2T7fVM/0.jpg' border='0' alt='0'/></a>       
-        <p>Hello, this is my beautiful profolio using react. It had all my hardwork of projects and chalenges!
-           Take a look! xoxo!      
-           
-           <p>
-           A little about me is that I am a newly coder who loves it, is 22, and manager at a Cafe.
-</p>
+          <a href="https://postimages.org/" target="blank">
+            <img src="https://i.postimg.cc/FH2T7fVM/0.jpg" border="0" alt="0" />
+          </a>
+          <p>
+            Hello, this is my beautiful portfolio using react. It had all my
+            hardwork of projects and chalenges! Take a look! xoxo!
+            <p>
+              A little about me is that I am a newly coder who loves it, is 22,
+              and manager at a Cafe.
             </p>
-       
-      <p>
-            Also if you want, view my  <a href="#/resume" class="link">resume</a>
-    </p>
-      </div>  
+          </p>
+
+          <p>
+            Also if you want, view my{' '}
+            <a href="#/resume" class="link">
+              resume
+            </a>
+          </p>
+        </div>
       </div>
-    </section> 
-)}
-  
+    </section>
+  );
+}
+
 export default About;
